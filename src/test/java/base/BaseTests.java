95943c4620ba0b16670b8c0c8cf87ffbf70a51f8
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-09-07 12:30:13 -0300

    Login tests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,16 +1,22 @@
+package base;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.HomePage;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
         driver.manage().window().maximize();
     }
 
@@ -18,6 +24,9 @@ public class BaseTests {
     public void tearDown(){
         driver.quit();
     }
+
+
+
 
 }
 
