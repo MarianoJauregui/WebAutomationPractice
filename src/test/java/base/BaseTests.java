eb715fd40c5f318bc49ebe00098544317b54f8be
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-09-07 10:12:16 -0300

    Starting Selenium and TestNG

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTests {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
+
